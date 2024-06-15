74eca55a41194313c8fccf5e124144118cf71031
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-15 22:37:14 +0545

    remove flutter

--- client/android/app/src/main/kotlin/com/example/utechseel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.utechseel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- client/lib/api/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class ApiConstant {
-  String baseUrl = dotenv.env['API_URL'] ?? 'http://localhost:3000';
-}
